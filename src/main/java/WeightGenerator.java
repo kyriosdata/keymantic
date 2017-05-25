8bddeca446e8227feff5ffdcfcc196834b272e58
Erivan <erivan.nas@gmail.com>
2017-05-25 16:34:56 -0300

    Atualiza classe

--- src/main/java/WeightGenerator.java
+++ src/main/java/WeightGenerator.java
@@ -3,12 +3,12 @@ import info.debatty.java.stringsimilarity.Levenshtein;
 
 public class WeightGenerator 
 {
-	public double[][] computeISW( String[] keywords, String[] archetypes )
+	public double[][] computeISW( String[] keywords, String[] archetypeTerms )
 	{		
-		double[][] matrix = new double[keywords.length][archetypes.length];
+		double[][] matrix = new double[keywords.length][archetypeTerms.length];
 		
 		for( int i = 0; i < keywords.length; i++ ){
-			for( int j = 0; j < archetypes.length; j++ ){
+			for( int j = 0; j < archetypeTerms.length; j++ ){
 				matrix[i][j] = 0.0;
 			}
 		}
@@ -18,16 +18,16 @@ public class WeightGenerator
 		
 		for( int i = 0; i < keywords.length; i++ )
 		{
-			for( int j = 0; j < archetypes.length; j++ )
+			for( int j = 0; j < archetypeTerms.length; j++ )
 			{
 				double similarity = 0;
 				
-					if( jaccard.similarity(keywords[i], archetypes[j]) > similarity ) {
-						similarity = jaccard.similarity(keywords[i], archetypes[j]) * 100;
+					if( jaccard.similarity(keywords[i], archetypeTerms[j]) > similarity ) {
+						similarity = jaccard.similarity(keywords[i], archetypeTerms[j]) * 100;
 					}
 					
-					if( levenshtein.distance(keywords[i], archetypes[j]) > similarity) {
-						similarity = levenshtein.distance(keywords[i], archetypes[j]) * 10;
+					if( levenshtein.distance(keywords[i], archetypeTerms[j]) > similarity) {
+						similarity = levenshtein.distance(keywords[i], archetypeTerms[j]) * 10;
 					}
 				
 				matrix[i][j] = similarity;
