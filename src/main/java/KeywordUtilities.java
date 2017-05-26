7668588df9acdecdad5aaba3e4ea6e06857fff2c
Erivan <erivan.nas@gmail.com>
2017-05-26 14:30:48 -0300

    Atualiza classe KeywordUtilities
    Adicionado método para obter os termos de arquétipos, uma vez aque essas informações são fixas e não precisam ser passadas por parametro.

--- src/main/java/KeywordUtilities.java
+++ src/main/java/KeywordUtilities.java
@@ -1,41 +1,47 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 
-public class KeywordUtilities 
-{
-	private String[] archetypes = {"adverse reaction list",
-			"care plan", "encounter", "event summary",
-			"family history", "health summary"};
-	
+public class KeywordUtilities {
+	private String[] archetypes = { "adverse reaction list", "care plan", "encounter", "event summary",
+			"family history", "health summary" };
+
 	private ArrayList<String> ontologies = new ArrayList<String>();
-	
-	public KeywordUtilities()
-	{
+
+	public KeywordUtilities() {
 		ontologies.add("at0000 " + "Family history"
 				+ "Summary information about the significant health-related problems found in family members.");
 	}
-	
-	public boolean isArchetypeName(String key)
-	{
+
+	public boolean isArchetypeName(String key) {
 		for (int i = 0; i < archetypes.length; i++) {
-			if( archetypes[i] == key )
+			if (archetypes[i] == key)
 				return true;
 		}
 		return false;
 	}
-	
-	public boolean isOntologyName(String key)
-	{
+
+	public boolean isOntologyName(String key) {
 		Iterator<String> iterador = ontologies.iterator();
-		
+
 		while (iterador.hasNext()) {
-			if( iterador.next().contains(key))
-			{
+			if (iterador.next().contains(key)) {
 				return true;
 			}
-			
 		}
-		
+
 		return false;
+	}
+
+	public String[] getArchetypeTerms() {
+		ArrayList<String> aux = (ArrayList<String>) Arrays.asList(archetypes);
+
+		Iterator<String> iterator = ontologies.iterator();
+
+		while (iterator.hasNext()) {
+			aux.add(iterator.next());
+		}
+
+		return (String[]) aux.toArray();
 	}
 }
