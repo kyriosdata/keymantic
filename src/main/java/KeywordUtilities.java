a27ffbe95577242c9f9d2381b2f0b58ec8f2c686
Erivan <erivan.nas@gmail.com>
2017-05-26 14:44:26 -0300

    Atualiza classe KeywordUtilities
    Lista de nomes de arquetipos agora é mantida em um arraylist

--- src/main/java/KeywordUtilities.java
+++ src/main/java/KeywordUtilities.java
@@ -3,20 +3,31 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class KeywordUtilities {
-	private String[] archetypes = { "adverse reaction list", "care plan", "encounter", "event summary",
-			"family history", "health summary" };
-
+	private ArrayList<String> archetypes = new ArrayList<String>();
+	
 	private ArrayList<String> ontologies = new ArrayList<String>();
 
 	public KeywordUtilities() {
+		
+		archetypes.add("adverse reaction list");
+		archetypes.add("care plan");
+		archetypes.add("encounter");
+		archetypes.add("event summary");
+		archetypes.add("family history");
+		archetypes.add("health summary");
+		
+		
 		ontologies.add("at0000 " + "Family history"
 				+ "Summary information about the significant health-related problems found in family members.");
 	}
 
 	public boolean isArchetypeName(String key) {
-		for (int i = 0; i < archetypes.length; i++) {
-			if (archetypes[i] == key)
+		Iterator<String> iterador = archetypes.iterator();
+
+		while (iterador.hasNext()) {
+			if (iterador.next().contains(key)) {
 				return true;
+			}
 		}
 		return false;
 	}
@@ -29,7 +40,6 @@ public class KeywordUtilities {
 				return true;
 			}
 		}
-
 		return false;
 	}
 
--- src/main/java/WeightGenerator.java
+++ src/main/java/WeightGenerator.java
@@ -1,39 +1,35 @@
 import info.debatty.java.stringsimilarity.Jaccard;
 import info.debatty.java.stringsimilarity.Levenshtein;
 
-public class WeightGenerator 
-{
-	public double[][] computeISW( String[] keywords )
-	{		
-		KeywordUtilities utilities = new KeywordUtilities();
-		
-		String[] archetypeTerms = utilities.getArchetypeTerms();
-		
+public class WeightGenerator {
+	KeywordUtilities utilities = new KeywordUtilities();
+	String[] archetypeTerms = utilities.getArchetypeTerms();
+
+	public double[][] computeISW(String[] keywords) {
+
 		double[][] matrix = new double[keywords.length][archetypeTerms.length];
-		
-		for( int i = 0; i < keywords.length; i++ ){
-			for( int j = 0; j < archetypeTerms.length; j++ ){
+
+		for (int i = 0; i < keywords.length; i++) {
+			for (int j = 0; j < archetypeTerms.length; j++) {
 				matrix[i][j] = 0.0;
 			}
 		}
-		
+
 		Jaccard jaccard = new Jaccard();
 		Levenshtein levenshtein = new Levenshtein();
-		
-		for( int i = 0; i < keywords.length; i++ )
-		{
-			for( int j = 0; j < archetypeTerms.length; j++ )
-			{
+
+		for (int i = 0; i < keywords.length; i++) {
+			for (int j = 0; j < archetypeTerms.length; j++) {
 				double similarity = 0;
-				
-					if( jaccard.similarity(keywords[i], archetypeTerms[j]) > similarity ) {
-						similarity = jaccard.similarity(keywords[i], archetypeTerms[j]) * 100;
-					}
-					
-					if( levenshtein.distance(keywords[i], archetypeTerms[j]) > similarity) {
-						similarity = levenshtein.distance(keywords[i], archetypeTerms[j]) * 10;
-					}
-				
+
+				if (jaccard.similarity(keywords[i], archetypeTerms[j]) > similarity) {
+					similarity = jaccard.similarity(keywords[i], archetypeTerms[j]) * 100;
+				}
+
+				if (levenshtein.distance(keywords[i], archetypeTerms[j]) > similarity) {
+					similarity = levenshtein.distance(keywords[i], archetypeTerms[j]) * 10;
+				}
+
 				matrix[i][j] = similarity;
 			}
 		}
