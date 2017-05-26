0babf045961dd3b14ad848a320c8951f5e902130
Erivan <erivan.nas@gmail.com>
2017-05-26 14:05:12 -0300

    atualiza classe KeywordUtilities

--- src/main/java/KeywordUtilities.java
+++ src/main/java/KeywordUtilities.java
@@ -1,8 +1,16 @@
 
 public class KeywordUtilities 
 {
+	private String[] archetypes = {"adverse reaction list",
+			"care plan", "encounter", "event summary",
+			"family history", "health summary"};
+	
 	public boolean isArchetypeName(String key)
 	{
+		for (int i = 0; i < archetypes.length; i++) {
+			if( archetypes[i] == key )
+				return true;
+		}
 		return false;
 	}
 	
