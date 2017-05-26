64ca98a444f31d943a73c8e564300042b4b9bc76
Erivan <erivan.nas@gmail.com>
2017-05-26 14:17:09 -0300

    Atualiza classe Keyword Utilities
    Adicionada implementação parcial do método isOntologyName valida para a ontologia Family History

--- src/main/java/KeywordUtilities.java
+++ src/main/java/KeywordUtilities.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class KeywordUtilities 
 {
 	private String[] archetypes = {"adverse reaction list",
 			"care plan", "encounter", "event summary",
 			"family history", "health summary"};
+	
+	private ArrayList<String> ontologies = new ArrayList<String>();
+	
+	public KeywordUtilities()
+	{
+		ontologies.add("at0000 " + "Family history"
+				+ "Summary information about the significant health-related problems found in family members.");
+	}
 	
 	public boolean isArchetypeName(String key)
 	{
@@ -16,6 +26,16 @@ public class KeywordUtilities
 	
 	public boolean isOntologyName(String key)
 	{
+		Iterator<String> iterador = ontologies.iterator();
+		
+		while (iterador.hasNext()) {
+			if( iterador.next().contains(key))
+			{
+				return true;
+			}
+			
+		}
+		
 		return false;
 	}
 }
